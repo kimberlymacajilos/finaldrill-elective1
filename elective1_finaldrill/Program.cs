3b42883efe4a1ef852c6b313b354cf09671ef1ab
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2023-12-10 21:12:39 +0800

    added getters and setters

---
+++ elective1_finaldrill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elective1_finaldrill
+{
+    public class Student
+    {
+        public int studentnumber { get; set; }
+        public string surname { get; set; }
+        public string firstname { get; set; }
+        public string occupation { get; set; }
+        public string gender { get; set; }
+        public int countrycode { get; set; }
+        public int areacode { get; set; }
+        public int phonenumber { get; set; }
+    }
+}
